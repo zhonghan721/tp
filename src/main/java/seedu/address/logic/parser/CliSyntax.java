f04720cd555de80cdc46595b1e4508c8408218fd
B-enguin <e0959432@u.nus.edu>
2023-10-12 08:29:39 +0800

    Close #106 Update CLI Syntax for project prefix format

--- src/main/java/seedu/address/logic/parser/CliSyntax.java
+++ src/main/java/seedu/address/logic/parser/CliSyntax.java
@@ -6,10 +6,20 @@ package seedu.address.logic.parser;
 public class CliSyntax {
 
     /* Prefix definitions */
-    public static final Prefix PREFIX_NAME = new Prefix("n/");
-    public static final Prefix PREFIX_PHONE = new Prefix("p/");
-    public static final Prefix PREFIX_EMAIL = new Prefix("e/");
-    public static final Prefix PREFIX_ADDRESS = new Prefix("a/");
-    public static final Prefix PREFIX_TAG = new Prefix("t/");
+
+    public static final Prefix PREFIX_USER = new Prefix("--user");
+
+    public static final Prefix PREFIX_PASSWORD = new Prefix("--password");
+
+    public static final Prefix PREFIX_PASSWORD_CONFIRM = new Prefix("--confirmPass");
+    public static final Prefix PREFIX_NAME = new Prefix("--name");
+    public static final Prefix PREFIX_PHONE = new Prefix("--phone");
+    public static final Prefix PREFIX_EMAIL = new Prefix("--email");
+    public static final Prefix PREFIX_ADDRESS = new Prefix("--address");
+    public static final Prefix PREFIX_CUSTOMER_ID = new Prefix("--customer");
+    public static final Prefix PREFIX_DATE = new Prefix("--date");
+    public static final Prefix PREFIX_SORT = new Prefix("--sort");
+    @Deprecated
+    public static final Prefix PREFIX_TAG = new Prefix("--tag");
 
 }
