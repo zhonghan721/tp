f1373bd3b7b0119e4aec66337038b35afcea8802
Julius Bryan <67666355+juliusgambe@users.noreply.github.com>
2023-10-14 09:44:16 +0800

    Remove customer import

--- src/test/java/seedu/address/model/delivery/DeliveryTest.java
+++ src/test/java/seedu/address/model/delivery/DeliveryTest.java
@@ -7,7 +7,6 @@ import static seedu.address.testutil.TypicalDeliveries.GAMBES_RICE;
 
 import org.junit.jupiter.api.Test;
 
-import seedu.address.model.person.Customer;
 import seedu.address.testutil.DeliveryBuilder;
 import seedu.address.testutil.TypicalPersons;
 
