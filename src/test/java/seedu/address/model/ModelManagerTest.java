6daacc04af0456afc1b4bbe81f59cee8d1de5b84
B-enguin <e0959432@u.nus.edu>
2023-10-16 17:40:49 +0800

    Add Testing for ModelManager, DeliveryNameContainsKeywordsPredicate

--- src/test/java/seedu/address/model/ModelManagerTest.java
+++ src/test/java/seedu/address/model/ModelManagerTest.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.model.Model.PREDICATE_SHOW_ALL_CUSTOMERS;
+import static seedu.address.model.Model.PREDICATE_SHOW_ALL_DELIVERIES;
 import static seedu.address.testutil.Assert.assertThrows;
 import static seedu.address.testutil.TypicalPersons.ALICE;
 import static seedu.address.testutil.TypicalPersons.BENSON;
@@ -17,6 +18,7 @@ import java.util.Arrays;
 import org.junit.jupiter.api.Test;
 
 import seedu.address.commons.core.GuiSettings;
+import seedu.address.model.delivery.DeliveryNameContainsKeywordsPredicate;
 import seedu.address.model.person.NameContainsKeywordsPredicate;
 import seedu.address.testutil.AddressBookBuilder;
 import seedu.address.testutil.DeliveryBookBuilder;
@@ -97,6 +99,40 @@ public class ModelManagerTest {
         assertThrows(UnsupportedOperationException.class, () -> modelManager.getFilteredPersonList().remove(0));
     }
 
+    // Delivery
+    @Test
+    public void setDeliveryBookFilePath_nullPath_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> modelManager.setDeliveryBookFilePath(null));
+    }
+
+    @Test
+    public void setDeliveryBookFilePath_validPath_setsAddressBookFilePath() {
+        Path path = Paths.get("delivery/book/file/path");
+        modelManager.setDeliveryBookFilePath(path);
+        assertEquals(path, modelManager.getDeliveryBookFilePath());
+    }
+
+    @Test
+    public void hasDelivery_nullDelivery_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> modelManager.hasDelivery(null));
+    }
+
+    @Test
+    public void hasDelivery_deliveryNotInAddressBook_returnsFalse() {
+        assertFalse(modelManager.hasDelivery(GABRIELS_MILK));
+    }
+
+    @Test
+    public void hasDelivery_deliveryInAddressBook_returnsTrue() {
+        modelManager.addDelivery(GABRIELS_MILK);
+        assertTrue(modelManager.hasDelivery(GABRIELS_MILK));
+    }
+
+    @Test
+    public void getFilteredDeliveryList_modifyList_throwsUnsupportedOperationException() {
+        assertThrows(UnsupportedOperationException.class, () -> modelManager.getFilteredDeliveryList().remove(0));
+    }
+
     @Test
     public void equals() {
         AddressBook addressBook = new AddressBookBuilder().withPerson(ALICE).withPerson(BENSON).build();
@@ -126,13 +162,21 @@ public class ModelManagerTest {
         // different deliverybook -> returns false
         assertFalse(modelManager.equals(new ModelManager(addressBook, differentDeliveryBook, userPrefs)));
 
-        // different filteredList -> returns false
+        // different filteredPersonList -> returns false
         String[] keywords = ALICE.getName().fullName.split("\\s+");
         modelManager.updateFilteredPersonList(new NameContainsKeywordsPredicate(Arrays.asList(keywords)));
         assertFalse(modelManager.equals(new ModelManager(addressBook, deliveryBook, userPrefs)));
 
         // resets modelManager to initial state for upcoming tests
         modelManager.updateFilteredPersonList(PREDICATE_SHOW_ALL_CUSTOMERS);
+
+        // different filteredDeliveryList -> returns false
+        String[] deliveryKeywords = GABRIELS_MILK.getName().deliveryName.split("\\s+");
+        modelManager.updateFilteredDeliveryList(new DeliveryNameContainsKeywordsPredicate(Arrays.asList(keywords)));
+        assertFalse(modelManager.equals(new ModelManager(addressBook, deliveryBook, userPrefs)));
+
+        // resets modelManager to initial state for upcoming tests
+        modelManager.updateFilteredDeliveryList(PREDICATE_SHOW_ALL_DELIVERIES);
 
         // different userPrefs -> returns false
         UserPrefs differentUserPrefs = new UserPrefs();
