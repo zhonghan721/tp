bd159d34661044f6ef9471f19ac07663d3f82e06
Gabriel4357 <gabrielseethor.jh@gmail.com>
2023-10-17 01:54:04 +0800

    Set up DeliveryAddCommand a little

--- src/main/java/seedu/address/logic/commands/delivery/DeliveryAddCommand.java
+++ src/main/java/seedu/address/logic/commands/delivery/DeliveryAddCommand.java
@@ -1,48 +1,44 @@
-package seedu.address.logic.commands.customer;
-
-import static java.util.Objects.requireNonNull;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_ADDRESS;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_EMAIL;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_PHONE;
+package seedu.address.logic.commands.delivery;
 
 import seedu.address.commons.util.ToStringBuilder;
 import seedu.address.logic.Messages;
 import seedu.address.logic.commands.CommandResult;
+import seedu.address.logic.commands.customer.AddCommand;
+import seedu.address.logic.commands.customer.CustomerCommand;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
+import seedu.address.model.delivery.Delivery;
 import seedu.address.model.person.Customer;
 
-/**
- * Adds a person to the address book.
- */
-public class AddCommand extends CustomerCommand {
+import static java.util.Objects.requireNonNull;
+import static seedu.address.logic.parser.CliSyntax.*;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_ADDRESS;
+
+public class DeliveryAddCommand extends DeliveryCommand {
 
     public static final String COMMAND_WORD = CustomerCommand.COMMAND_WORD + " " + "add";
 
-    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Adds a customer to the HomeBoss database. "
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Adds a Delivery to the HomeBoss database. "
             + "Parameters: "
-            + PREFIX_NAME + "NAME "
-            + PREFIX_PHONE + "PHONE "
-            + PREFIX_EMAIL + "EMAIL "
-            + PREFIX_ADDRESS + "ADDRESS\n"
+            + "DELIVERY_NAME"
+            + PREFIX_CUSTOMER_ID + "CUSTOMER_ID "
+            + PREFIX_DATE + "DATE\n "
             + "Example: " + COMMAND_WORD + " "
-            + PREFIX_NAME + "John Doe "
-            + PREFIX_PHONE + "98765432 "
-            + PREFIX_EMAIL + "johnd@example.com "
-            + PREFIX_ADDRESS + "311, Clementi Ave 2, #02-25";
+            + "furniture "
+            + PREFIX_CUSTOMER_ID + "5 "
+            + PREFIX_DATE + "2023-12-03 ";
 
-    public static final String MESSAGE_SUCCESS = "New customer added: %1$s";
-    public static final String MESSAGE_DUPLICATE_PERSON = "This customer already exists in HomeBoss";
+    public static final String MESSAGE_SUCCESS = "New delivery added: %1$s";
+    public static final String MESSAGE_DUPLICATE_DELIVERY = "This delivery already exists in HomeBoss";
 
-    private final Customer toAdd;
+    private final Delivery toAdd;
 
     /**
      * Creates an AddCommand to add the specified {@code Customer}
      */
-    public AddCommand(Customer customer) {
-        requireNonNull(customer);
-        toAdd = customer;
+    public DeliveryAddCommand(Delivery delivery) {
+        requireNonNull(delivery);
+        toAdd = delivery;
     }
 
     @Override
@@ -78,4 +74,6 @@ public class AddCommand extends CustomerCommand {
                 .add("toAdd", toAdd)
                 .toString();
     }
+}
+
 }
---
+++ src/main/java/seedu/address/logic/commands/delivery/DeliveryCommand.java
@@ -0,0 +1,11 @@
+package seedu.address.logic.commands.delivery;
+
+import seedu.address.logic.commands.Command;
+
+/**
+ * Represents a delivery command with hidden internal logic and the ability to be executed.
+ */
+public abstract class DeliveryCommand extends Command {
+
+    public static final String COMMAND_WORD = "delivery";
+}
