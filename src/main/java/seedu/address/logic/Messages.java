130ff3a955f60d34dbffae6867c1a50356d43442
zhonghan721 <e0957606@u.nus.edu>
2023-11-11 20:14:07 +0800

    Improve code quality

--- src/main/java/seedu/address/logic/Messages.java
+++ src/main/java/seedu/address/logic/Messages.java
@@ -22,7 +22,7 @@ public class Messages {
     public static final String MESSAGE_CUSTOMERS_MATCHED_LISTED = "%1$d customers listed!";
     public static final String MESSAGE_DELIVERY_LISTED_OVERVIEW = "%1$d deliveries listed!";
     public static final String MESSAGE_DUPLICATE_FIELDS = "Multiple values specified for the following"
-        + "single-valued field(s): ";
+        + " single-valued field(s): ";
     public static final String MESSAGE_USER_NOT_AUTHENTICATED = "Access denied! You are currently not logged in.";
     public static final String MESSAGE_WELCOME = "Welcome to HomeBoss!\n\n" + "You are currently logged out.\n"
         + "Login or register to start using HomeBoss.";
--- src/test/java/seedu/address/logic/commands/DeliveryAddCommandTest.java
+++ src/test/java/seedu/address/logic/commands/DeliveryAddCommandTest.java
@@ -275,6 +275,14 @@ public class DeliveryAddCommandTest {
         }
 
         /**
+         * Resets the customer list to show no customers.
+         */
+        @Override
+        public void clearFilteredCustomerList() {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        /**
          * Returns the number of customers in the filtered customer list.
          *
          * @return the number of customers in the filtered customer list.
@@ -349,6 +357,14 @@ public class DeliveryAddCommandTest {
          */
         @Override
         public void showAllFilteredDeliveryList() {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        /**
+         * Resets the delivery list to show no deliveries.
+         */
+        @Override
+        public void clearFilteredDeliveryList() {
             throw new AssertionError("This method should not be called.");
         }
 
