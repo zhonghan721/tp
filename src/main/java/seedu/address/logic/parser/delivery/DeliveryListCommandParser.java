3903ec493f794dcd21eac75092afe3c914ebb98d
Julius Bryan <67666355+juliusgambe@users.noreply.github.com>
2023-11-11 22:59:20 +0800

    Remove duplicate checker

--- src/main/java/seedu/address/logic/parser/delivery/DeliveryListCommandParser.java
+++ src/main/java/seedu/address/logic/parser/delivery/DeliveryListCommandParser.java
@@ -36,8 +36,7 @@ public class DeliveryListCommandParser implements Parser<DeliveryListCommand> {
             throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT,
                 DeliveryListCommand.MESSAGE_USAGE));
         }
-
-        argMultimap.verifyNoDuplicatePrefixesFor(PREFIX_STATUS, PREFIX_SORT, PREFIX_CUSTOMER_ID, PREFIX_DATE);
+        
         Optional<String> sort = argMultimap.getValue(PREFIX_SORT);
         Optional<String> inputStatus = argMultimap.getValue(PREFIX_STATUS);
         Optional<String> inputCustomerId = argMultimap.getValue(PREFIX_CUSTOMER_ID);
