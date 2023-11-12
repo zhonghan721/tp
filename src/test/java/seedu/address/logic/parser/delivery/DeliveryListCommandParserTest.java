577f7211c5accb8e051d0a0bfefac25a9b263f03
Julius Bryan <67666355+juliusgambe@users.noreply.github.com>
2023-11-13 00:28:25 +0800

    Fix imports

--- src/test/java/seedu/address/logic/parser/delivery/DeliveryListCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/delivery/DeliveryListCommandParserTest.java
@@ -30,7 +30,6 @@ import seedu.address.logic.Sort;
 import seedu.address.logic.commands.delivery.DeliveryListCommand;
 import seedu.address.logic.commands.delivery.DeliveryViewCommand;
 import seedu.address.logic.parser.CommandParserTestUtil;
-import seedu.address.logic.parser.DeliveryListParser;
 import seedu.address.logic.parser.ParserUtil;
 import seedu.address.model.delivery.Date;
 import seedu.address.model.delivery.DeliveryDate;
