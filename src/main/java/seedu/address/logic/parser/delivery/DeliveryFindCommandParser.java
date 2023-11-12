b57c02df418e11f323c55153f6be61ef37dc9886
B-enguin <e0959432@u.nus.edu>
2023-11-12 23:54:39 +0800

    Add logging and assertions
    Add for the following files,
    - CustomerViewParser
    - DeliveryCreateNoteParser
    - DeliveryFindParser
    - DeliveryStatusParser

--- src/main/java/seedu/address/logic/parser/delivery/DeliveryFindCommandParser.java
+++ src/main/java/seedu/address/logic/parser/delivery/DeliveryFindCommandParser.java
@@ -3,6 +3,7 @@ package seedu.address.logic.parser.delivery;
 import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 
 import java.util.Arrays;
+import java.util.logging.Logger;
 
 import seedu.address.logic.commands.delivery.DeliveryFindCommand;
 import seedu.address.logic.parser.Parser;
@@ -14,6 +15,8 @@ import seedu.address.model.delivery.DeliveryNameContainsKeywordsPredicate;
  */
 public class DeliveryFindCommandParser implements Parser<DeliveryFindCommand> {
 
+    private static final Logger logger = Logger.getLogger(DeliveryFindCommandParser.class.getName());
+
     /**
      * Parses the given {@code String} of arguments in the context of the DeliveryFindCommand
      * and returns a DeliveryFindCommand object for execution.
@@ -22,11 +25,14 @@ public class DeliveryFindCommandParser implements Parser<DeliveryFindCommand> {
     public DeliveryFindCommand parse(String args) throws ParseException {
         String trimmedArgs = args.trim();
         if (trimmedArgs.isEmpty()) {
+            logger.warning("DeliveryFindCommand: empty arguments given");
             throw new ParseException(
                 String.format(MESSAGE_INVALID_COMMAND_FORMAT, DeliveryFindCommand.MESSAGE_USAGE));
         }
 
         String[] nameKeywords = trimmedArgs.split("\\s+");
+
+        assert Arrays.stream(nameKeywords).anyMatch(s -> !s.isEmpty()) : "Keywords cannot be empty";
 
         return new DeliveryFindCommand(new DeliveryNameContainsKeywordsPredicate(Arrays.asList(nameKeywords)));
     }
